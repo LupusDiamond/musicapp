e806dbb6824763923d063ee0a73666260df1296e
LupusDiamond <godzplayer@hotmail.com>
2019-09-29 23:48:03 +0300

    commmit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-window.addEventListener("load", () => {
-  const sounds = document.querySelectorAll(".sound");
-  const pads = document.querySelectorAll(".pads div");
-  const visual = document.querySelector(".visual");
+window.addEventListener('load', () => {
+  const sounds = document.querySelectorAll('.sound');
+  const pads = document.querySelectorAll('.pads div');
+  const visual = document.querySelector('.visual');
+
   const colors = [
     "#60d394",
     "#d36060",
@@ -11,22 +12,22 @@ window.addEventListener("load", () => {
     "#60c2d3"
   ];
 
+  // Let's get going with the sound!
   pads.forEach((pad, index) => {
-    pad.addEventListener("click", function() {
+    pad.addEventListener('click', function () {
       sounds[index].currentTime = 0;
       sounds[index].play();
-      createBubble(index);
+      createBubbles(index);
     });
   });
-
-  const createBubble = index => {
-    //Create bubbles
-    const bubble = document.createElement("div");
+  // Create a function that makes bubbles!
+  const createBubbles = (index) => {
+    const bubble = document.createElement('div');
     visual.appendChild(bubble);
     bubble.style.backgroundColor = colors[index];
-    bubble.style.animation = `jump 1s ease`;
-    bubble.addEventListener("animationend", function() {
+    bubble.style.animation = 'jump 1s ease';
+    bubble.addEventListener('animationend', function () {
       visual.removeChild(this);
-    });
-  };
+    })
+  }
 });
